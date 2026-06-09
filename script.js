b1296d2fda430df7d979a13fd71fc1ad38ca1ea7
wawa2016-here <lemver2016@outlook.com>
2026-06-08 19:48:11 -0500

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-let timeInMinutes = 10;
-let timeInSeconds = 59;
+let totalSeconds = 10 * 60;
 
-setInterval(() => { 
-  let element = document.getElementById("Minutes");
-    document.getElementById("Minutes").innerHTML = " " + timeInMinutes + " "; 
-  }
-}, 60000); 
+const timerInterval = setInterval(() => {
+    
+    totalSeconds--;
+
+    
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+
+    
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    
+    document.getElementById("Minutes").innerHTML = minutes + ":" + seconds;
+
+    
+    if (totalSeconds <= 0) {
+        clearInterval(timerInterval);
+        document.getElementById("Minutes").innerHTML = "Time's Up!";
+    }
+}, 1000); 
