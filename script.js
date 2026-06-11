e6f3098eb89382f35049935f411c670283385907
Wawa2016-here <lemver2016@outlook.com>
2026-06-11 15:08:03 -0500

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 let totalSeconds = 10 * 60;
 
-const timerInterval = setInterval(() => {
-    
+function updateTimer() {
     totalSeconds--;
 
-    
     let minutes = Math.floor(totalSeconds / 60);
     let seconds = totalSeconds % 60;
 
-    
     if (seconds < 10) {
         seconds = "0" + seconds;
     }
 
-    
     document.getElementById("Minutes").innerHTML = minutes + ":" + seconds;
 
-    
     if (totalSeconds <= 0) {
         clearInterval(timerInterval);
         document.getElementById("Minutes").innerHTML = "Time's Up!";
     }
-}, 1000); 
+}
+
+const timerInterval = setInterval(updateTimer, 1000); 
